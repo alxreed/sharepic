be00d97ad149c17c34ea4cac536771c3ce8511fe
Alex <ch.alexandre.reed@gmail.com>
2020-02-04 10:35:38 +0100

    add user in db ok

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -9,11 +9,13 @@ export class UserService {
   constructor(private db: AngularFirestore) { }
 
   addUserInDB(event: { email: any; firstname: any; lastname: any; avatarUrl: any; }) {
-    this.db.collection<any>('users').add({
+    const id = this.db.createId();
+    const user = {
       email: event.email,
       firstname: event.firstname,
       lastname: event.lastname,
       avatarUrl: event.avatarUrl,
-    });
+    };
+    this.db.collection<any>('users').doc(id).set(user);
   }
 }

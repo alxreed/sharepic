49cba41bea1935b31930baaec03f99ef9d3d11fa
Alexandre Reed <ch.alexandre.reed@gmail.com>
2020-02-26 09:53:59 +0100

    feat(conversations): querying conversations works on current user

--- src/app/services/conversation.service.ts
+++ src/app/services/conversation.service.ts
@@ -11,18 +11,6 @@ export class ConversationService {
   getAllConversations(user) {
     console.log(user);
 
-    const member = {
-      firstname: user.firstname,
-      lastname: user.lastname,
-      avatarUrl: user.avatarUrl,
-      createdAt: user.createdAt,
-      email: user.email,
-    };
-
-    console.log(member);
-
-
-    return this.db.collection('conversations', ref => ref.where('members', 'array-contains', member)).valueChanges();
-    return this.db.collection('conversations').valueChanges();
+    return this.db.collection('conversations', ref => ref.where('members', 'array-contains', {email: user.email})).valueChanges();
   }
 }
